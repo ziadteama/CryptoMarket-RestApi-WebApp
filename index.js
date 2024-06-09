a180da28a6cff38800873d5a3c3953442e1037ff
Ziad Teama <ziadbasaria600@gmail.com>
2024-06-09 16:38:01 +0300

    data fetched successfully

--- index.js
+++ index.js
@@ -1,28 +1,51 @@
 // index.mjs
-import express from 'express';
-import bodyParser from 'body-parser';
-import axios from 'axios';
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+
+function sortDataBySymbol(data) {
+    data.sort((a, b) => {
+        if (a.symbol < b.symbol) {
+            return -1;
+        }
+        if (a.symbol > b.symbol) {
+            return 1;
+        }
+        return 0;
+    });
+
+    return data;
+}
+
 
 const app = express();
 const port = 3000;
-const apiKey="f56b055c-d20d-40af-8636-3a849421f78e";
+const apiKey = "f56b055c-d20d-40af-8636-3a849421f78e";
 const config = {
-    headers: { 'X-API-Token': apiKey },
-  };
-// Body parser middleware
+  headers: { "X-API-Token": apiKey },
+};
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(express.static("public"));
 
-// Set the view engine to EJS
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-// Define a route that renders the index.ejs template
-app.get('/', (req, res) => {
-    // Pass data to the template
-    res.render('index.ejs');
+app.get("/", async (req, res) => {
+  try {
+    const response = await axios.get(
+      "https://api.blockchain.com/v3/exchange/tickers",
+      config
+    );
+    const data = sortDataBySymbol(response.data);
+    res.render("index.ejs", { cryptocurrencies: data });
+  } catch (error) {
+    res
+      .status(500)
+      .send("Error fetching cryptocurrency data. Please try again later.");
+  }
 });
 
-// Start the server
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
